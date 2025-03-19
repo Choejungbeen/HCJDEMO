f4b1a1ff5962d3965b1f0b49b209dd08f8e8128a
unknown <Choejungbeen>
2025-03-19 12:55:00 +0900

    1st

--- main.js
+++ main.js
@@ -15,20 +15,19 @@ function reset() {
 }
 
 function showhtml() {
-  document.getElementById('fig').src =
-    'https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/180px-HTML5_logo_and_wordmark.svg.png'
+  document.getElementById('fig').src = 'img/html5.PNG'
   document.getElementById('desc').innerHTML =
     '<b>HTML</b>은 구조적 웹문서를 작성하는데 사용하는 마크업 언어이다.'
 }
 
 function showcss() {
-  document.getElementById('fig').src = 'C:Users\bean0Desktop웹프자/css.img'
+  document.getElementById('fig').src = 'img/css3.PNG'
   document.getElementById('desc').innerHTML =
     '<b>CSS3</b>는 HTML 문서의 스타일을 지정하기 위한 언어이다.'
 }
 
 function showjs() {
-  document.getElementById('fig').src = 'img/javascript.jpg'
+  document.getElementById('fig').src = 'img/javascript.png'
   document.getElementById('desc').innerHTML =
     '<b>Javascript</b>는 웹을 위한 프로그래밍 언어이며 웹문서에서 동작을 실행할 수 있게 한다. '
 }
