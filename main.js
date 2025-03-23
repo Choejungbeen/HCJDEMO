b58e94219eae1d45c3e6c3f085dd9175b767cfe4
Choejungbeen <bean040949@gmail.com>
2025-03-23 20:59:46 +0900

    2nd

--- main.js
+++ main.js
@@ -31,8 +31,24 @@ function showjs() {
   document.getElementById('desc').innerHTML =
     '<b>Javascript</b>는 웹을 위한 프로그래밍 언어이며 웹문서에서 동작을 실행할 수 있게 한다. '
 }
+function showprofile1() {
+  document.getElementById('desc').innerHTML =
+    '미니언 BOB을 맡은 20학번 최정빈입니다! '
+}
+function showprofile2() {
+  document.getElementById('desc').innerHTML =
+    '미니언 Stuart을 맡은 18학번 송명석입니다! '
+}
+function showprofile3() {
+  document.getElementById('desc').innerHTML =
+    '미니언 Kevin을 맡은 00학번 신준혁입니다! '
+}
+function showprofile4() {
+  document.getElementById('desc').innerHTML =
+    '미니언 Otto을 맡은 00학번 김용섭입니다! '
+}
 
 function hide() {
-  document.getElementById('fig').src = ''
   document.getElementById('desc').innerHTML = ''
+  document.getElementById('fig').src = ''
 }
